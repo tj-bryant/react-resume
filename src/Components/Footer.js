789f4fd409188d9cd152538e8af5f2abde357af6
tjbryant <tjbryant@uncc.edu>
2020-05-10 11:34:39 -0400

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+
+    if(this.props.data){
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><img src={network.file} style={{height: 40}}></img></a></li>
+      })
+    }
+
+    return (
+      <footer>
+
+     <div className="row">
+        <div className="twelve columns">
+           <ul className="social-links">
+              {networks}
+           </ul>
+
+           <ul className="copyright">
+              <li>&copy; Copyright 2020 TJ Bryant</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+           </ul>
+
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+  </footer>
+    );
+  }
+}
+
+export default Footer;
